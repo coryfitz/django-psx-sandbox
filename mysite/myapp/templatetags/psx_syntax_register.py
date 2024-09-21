f7bc889a8c9e0c3bf73dd1d07cb15e5c850ab6fb
Cory Fitz <coryfitz@Corys-MBP.communityfibre.co.uk>
2024-09-21 23:51:20 +0100

    update

--- mysite/myapp/templatetags/psx_syntax_register.py
+++ mysite/myapp/templatetags/psx_syntax_register.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+from django.conf import settings
 from django import template
 from packed import Elem, Component, psx_import
 
@@ -6,7 +8,7 @@ register = template.Library()
 @register.simple_tag
 def psx_syntax():
 
-    psx_file_path = '/Users/coryfitz/Desktop/Framework/packed-test/mysite/myapp/templatetags/psx_main.psx'
+    psx_file_path = Path(settings.BASE_DIR) / 'myapp' / 'templatetags' / 'psx_main.psx'
 
     psx_syntax = psx_import(psx_file_path, 'psx_syntax')
 
